2510a8898d4526ebd75e7c9f65ea48852f1afc12
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-27 08:12:30 +0530

    Add files via upload

---
+++ interview_class_method.py
@@ -0,0 +1,17 @@
+class besanth():
+    def tec(self,name,age,gender,school):
+        self.name=name
+        self.age = age
+        self.gender = gender
+        self.school = school
+        print('name is yogi')
+
+    def tech(self):
+            print("name is",self.name)
+            print("age is 30",self.age)
+            print("gender is",self.gender)
+            print("school is besanth",self.school)
+obj=besanth()
+obj.tec('yogi',30,'male','besanth')
+obj.tech()
+
